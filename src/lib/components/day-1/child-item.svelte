8fdbeee0edec2b351d3b80109f9a9c494c1162e2
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2025-07-29 08:57:44 +0700

    Add Badge component for displaying status indicators in ChildItem; update ChildItem and ChildList for improved layout and styling

--- src/lib/components/day-1/child-item.svelte
+++ src/lib/components/day-1/child-item.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
+	import Badge from "../ui/badge/badge.svelte";
+	import BadgeCheckIcon from "@lucide/svelte/icons/badge-check";
+
 	let { name = 'Unnamed Child', tally = 0, ...restProps } = $props()
 	
 	// Use default name if the provided name is empty or falsy
 	const displayName = name || 'Unnamed Child';
 </script>
 
-<div {...restProps}>
-	<span data-testid="name">{displayName}</span>
-	<span data-testid="tally">{Math.round(tally)}</span>
+<div class="py-3 px-4 border border-gray-300 rounded-md" {...restProps}>
+	<div class="flex justify-between items-center">
+		<span data-testid="name">{displayName}</span>
+		<span data-testid="tally">{Math.round(tally)}</span>
+	</div>
 	{#if tally < 0}
-		<span data-testid="naughty-badge">Naughty</span>
+		<Badge variant="destructive" class="mt-2" data-testid="naughty-badge">Naughty</Badge>
 	{:else}
-		<span data-testid="nice-badge">Nice</span>
+		<Badge variant='default' class="mt-2 bg-blue-500 text-white" data-testid="nice-badge">
+			<BadgeCheckIcon />
+			Nice</Badge>
 	{/if}
 </div>
--- src/lib/components/day-1/child-list.svelte
+++ src/lib/components/day-1/child-list.svelte
@@ -4,8 +4,10 @@
     let { childs } = $props()
 </script>
 
+<div class="space-y-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 {#each childs as child, index}
     <ChildItem data-testid="child-item-{index}" name={child.name} tally={child.tally} />
 {:else}
     <div data-testid="empty-child">No children</div>
 {/each}
+</div>
